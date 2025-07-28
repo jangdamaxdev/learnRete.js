f90707b005269fbc7d3f28e6d6ff0992ec21cea2
WSL2 <Jangdamax.dev@gmail.com>
2025-07-29 05:43:41 +0700

    thêm lại baseURL: '/learnretejs/'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -111,7 +111,7 @@ export default defineNuxtConfig({
     defaultLocale: 'en',
   },
   app: {
-    // baseURL: '/learnretejs/',
+    baseURL: '/learnretejs/',
     head: {
       viewport: 'width=device-width,initial-scale=1',
       title: 'Rete.js',
