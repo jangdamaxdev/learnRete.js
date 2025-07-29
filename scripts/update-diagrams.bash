50f6db06e7f5088bb1011caae37ccc0b92095849
WSL2 <Jangdamax.dev@gmail.com>
2025-07-29 13:37:11 +0700

    dest_dir="./diagrams/" và mkdir

--- scripts/update-diagrams.bash
+++ scripts/update-diagrams.bash
@@ -1,7 +1,7 @@
 set -e
 
 source_dir="./src/assets/diagrams/"
-dest_dir="./learnretejs/diagrams/"
+dest_dir="./diagrams/"
 mkdir -p "$dest_dir"
 docker run $1 -v $(pwd):/data rlespinasse/drawio-export -f svg --remove-page-suffix -o _export
 
