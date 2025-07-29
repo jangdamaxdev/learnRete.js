8ac31de4f3960a5536f882c1042b70a1d5e18536
WSL2 <Jangdamax.dev@gmail.com>
2025-07-29 11:10:13 +0700

    xóa luôn base. không dùng cache@v3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -111,7 +111,6 @@ export default defineNuxtConfig({
     defaultLocale: 'en',
   },
   app: {
-    // baseURL: '/learnretejs/',
     head: {
       viewport: 'width=device-width,initial-scale=1',
       title: 'Rete.js',
