272bb74f2a1f7a619d8187c45855f5fb4bd0f0b0
WSL2 <Jangdamax.dev@gmail.com>
2025-07-29 11:30:29 +0700

    baseURL và ./dist

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -111,6 +111,7 @@ export default defineNuxtConfig({
     defaultLocale: 'en',
   },
   app: {
+    baseURL: '/learnretejs/',
     head: {
       viewport: 'width=device-width,initial-scale=1',
       title: 'Rete.js',
